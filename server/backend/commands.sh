935e712877d64945699b030f819e34254dd9e7e0
ebramkw <ebramkw@gmail.com>
2022-05-18 10:56:56 +0800

    updating commands.sh file

--- server/backend/commands.sh
+++ server/backend/commands.sh
@@ -23,10 +23,10 @@ do
     serial='.serial'
     stag_serial='.stag.serial'
     stag_serial_lr='.stag.serial.lr'
-    username='username@'
+    cirlab='cirlab@'
     ser2net='ser2net_'
     conf='.conf'
-    location=':/home/username/indriya_upgrade/'
+    location=':/home/cirlab/indriya_upgrade/'
     bin_location='telosb_bin/'
     bak='.bak'
 
@@ -56,10 +56,10 @@ do
     #echo ""
     #echo "*** SCP BINARIES ***"
 
-    #echo "Will execute: scp ~/contiki-2.7/examples/hello-world/*sky* username@mac-mini-com1-l1-mac:/home/username/indriya_upgrade/telosb_bin/"
-    #scp ~/contiki-2.7/examples/hello-world/*sky* username@mac-mini-com1-b-mac:/home/username/indriya_upgrade/telosb_bin/
-    #echo "Will execute: scp ~/contiki-2.7/examples/hello-world/*sky* username@mac-mini-com1-l1-el:/home/username/indriya_upgrade/telosb_bin/"
-    #scp ~/contiki-2.7/examples/hello-world/*sky* username@mac-mini-com1-b-el:/home/username/indriya_upgrade/telosb_bin/
+    #echo "Will execute: scp ~/contiki-2.7/examples/hello-world/*sky* cirlab@mac-mini-com1-l1-mac:/home/cirlab/indriya_upgrade/telosb_bin/"
+    #scp ~/contiki-2.7/examples/hello-world/*sky* cirlab@mac-mini-com1-b-mac:/home/cirlab/indriya_upgrade/telosb_bin/
+    #echo "Will execute: scp ~/contiki-2.7/examples/hello-world/*sky* cirlab@mac-mini-com1-l1-el:/home/cirlab/indriya_upgrade/telosb_bin/"
+    #scp ~/contiki-2.7/examples/hello-world/*sky* cirlab@mac-mini-com1-b-el:/home/cirlab/indriya_upgrade/telosb_bin/
 
 
 
@@ -70,7 +70,7 @@ do
     # NEXT RECORD FROM mac-mini-com1-l1-el.serial
 
     ###################################
-    ### serial file can be generated with ssh username@mac-mini-com1-l1-mac 'ls /dev/serial/by-id'
+    ### serial file can be generated with ssh cirlab@mac-mini-com1-l1-mac 'ls /dev/serial/by-id'
     ###
     ###################################
     #echo ""
@@ -79,21 +79,21 @@ do
     # +------------+--------------+-------------+
     # | moteTypeID | moteTypeName | runningTime |
     # +------------+--------------+-------------+
-    # |          7 | cc2650       |       0 |
-    # |          8 | telosb       |        0 |
+    # |          7 | cc2650       |       32717 |
+    # |          8 | telosb       |        2585 |
     # |          9 | cc1350       |           0 |
     # +------------+--------------+-------------+
 
     # +-----------+-------------+------------+
     # | clusterID | clusterName | floorLevel |
     # +-----------+-------------+------------+
-    # |         1 | NAME         | COM1#2     |
-    # |         2 | NAME     | COM2#B     |
-    # |         3 | NAME         | COM1#2     |
-    # |         4 | NAME         | COM1#1     |
-    # |         5 | NAME          | COM1#1     |
-    # |         6 | NAME          | COM1#B     |
-    # |         7 | NAME         | COM1#B     |
+    # |         1 | SR1         | COM1#2     |
+    # |         2 | cir_lab     | COM2#B     |
+    # |         3 | VCR         | COM1#2     |
+    # |         4 | NCL         | COM1#1     |
+    # |         5 | AI          | COM1#1     |
+    # |         6 | EL          | COM1#B     |
+    # |         7 | MAC         | COM1#B     |
     # +-----------+-------------+------------+
 
 
@@ -104,15 +104,16 @@ do
     # clear json file
     echo "clear json file"
     echo "" > ocean.json
+    echo "{" > ocean.json
 
     # clear motes from DB
-    echo "DELETE from motes;" | mysql -uroot -p ...;
+    echo "DELETE from motes;" | mysql -uroot -pNUS_SOC_cirlab_indriya-2017 indriyaDB;
 
     let "ID = 1";
     while read mac_mini;
             read mac_mini_db_id;
             do echo "mac-mini: "$mac_mini;
-            ssh=$username$mac_mini
+            ssh=$cirlab$mac_mini
             conf_file_name=$ser2net$mac_mini$conf
             bak_conf_file_name=$conf_file_name$bak
             ssh_location=$ssh$location
@@ -135,15 +136,16 @@ do
             while read dev;
                     do echo "id: "$ID;
                     echo "dev: "$dev;
-                    echo "ssh $ssh '/home/username/indriya_upgrade/burn_telosb_test.py telosb $dev /home/username/indriya_upgrade/telosb_bin/burn-nodeid.sky.$ID' &";
-                    ssh $ssh "/home/username/indriya_upgrade/burn_telosb_test.py telosb $dev /home/username/indriya_upgrade/telosb_bin/burn-nodeid.sky.$ID" &
+                    echo "ssh $ssh '/home/cirlab/indriya_upgrade/burn_telosb_test.py telosb $dev /home/cirlab/indriya_upgrade/telosb_bin/burn-nodeid.sky.$ID' &";
+                    ssh $ssh "/home/cirlab/indriya_upgrade/burn_telosb_test.py telosb $dev /home/cirlab/indriya_upgrade/telosb_bin/burn-nodeid.sky.$ID" &
                     echo "\"$ID\":{\"gateway\":\"$mac_mini\",\"port\":$port,\"serial_id\":\"$dev\",\"flash_file\":\"\"}, ==> ocean.json"
                     echo "\"$ID\":{\"gateway\":\"$mac_mini\",\"port\":$port,\"serial_id\":\"$dev\",\"flash_file\":\"\"}," >> ocean.json
                     echo "$port:raw:0:$dev:115200,8DATABITS,NONE,1STOPBIT ==> $conf_file_name"
                     echo "$port:raw:0:$dev:115200,8DATABITS,NONE,1STOPBIT" >> $conf_file_name
                     # echo "DELETE from motes WHERE virtual_id = $ID; ==> DB"
+                    # echo "DELETE from motes WHERE virtual_id = $ID;" | mysql -uroot -pNUS_SOC_cirlab_indriya-2017 indriyaDB;
                     # echo "INSERT INTO motes (virtual_id, moteTypes_moteTypeID, clusters_clusterID) values ($ID, 8, $mac_mini_db_id); ==> DB"
-                    echo "INSERT INTO motes (virtual_id, moteTypes_moteTypeID, clusters_clusterID) values ($ID, 8, $mac_mini_db_id);" | mysql -uroot -pPASSWORD DATABASE_NAME;
+                    echo "INSERT INTO motes (virtual_id, moteTypes_moteTypeID, clusters_clusterID) values ($ID, 8, $mac_mini_db_id);" | mysql -uroot -pNUS_SOC_cirlab_indriya-2017 indriyaDB;
                     let "ID++";
                     let "port++";
             done < $file_name
@@ -159,13 +161,14 @@ do
                     usb=`echo $usb |sed 's/.\{5\}$//'`
                     echo $usb;
 
-                    echo "\"$stag_id\":{\"gateway\":\"$mac_mini\",\"port\":$port,\"serial_id\":\"$dev\",\"flash_file\":\"/home/username/sensortag_configs/$usb.ccxml\"}, ==> ocean.json"
-            echo "\"$stag_id\":{\"gateway\":\"$mac_mini\",\"port\":$port,\"serial_id\":\"$dev\",\"flash_file\":\"/home/username/sensortag_configs/$usb.ccxml\"}," >> ocean.json
+                    echo "\"$stag_id\":{\"gateway\":\"$mac_mini\",\"port\":$port,\"serial_id\":\"$dev\",\"flash_file\":\"/home/cirlab/sensortag_configs/$usb.ccxml\"}, ==> ocean.json"
+            echo "\"$stag_id\":{\"gateway\":\"$mac_mini\",\"port\":$port,\"serial_id\":\"$dev\",\"flash_file\":\"/home/cirlab/sensortag_configs/$usb.ccxml\"}," >> ocean.json
             echo "$port:raw:0:$dev:115200,8DATABITS,NONE,1STOPBIT ==> $conf_file_name"
             echo "$port:raw:0:$dev:115200,8DATABITS,NONE,1STOPBIT" >> $conf_file_name
             # echo "DELETE from motes WHERE virtual_id = $stag_id; ==> DB";
+            # echo "DELETE from motes WHERE virtual_id = $stag_id;" | mysql -uroot -pNUS_SOC_cirlab_indriya-2017 indriyaDB;
             # echo "INSERT INTO motes (virtual_id, moteTypes_moteTypeID, clusters_clusterID) values ($stag_id, 7, $mac_mini_db_id); ==> DB"
-            echo "INSERT INTO motes (virtual_id, moteTypes_moteTypeID, clusters_clusterID) values ($stag_id, 7, $mac_mini_db_id);" | mysql -uroot -pPASSWORD DATABASE_NAME;
+            echo "INSERT INTO motes (virtual_id, moteTypes_moteTypeID, clusters_clusterID) values ($stag_id, 7, $mac_mini_db_id);" | mysql -uroot -pNUS_SOC_cirlab_indriya-2017 indriyaDB;
             let "port++";
         done < $file_name
         echo ""
@@ -180,13 +183,14 @@ do
                     usb=`echo $usb |sed 's/.\{5\}$//'`
                     echo $usb;
 
-                    echo "\"$stag_id\":{\"gateway\":\"$mac_mini\",\"port\":$port,\"serial_id\":\"$dev\",\"flash_file\":\"/home/username/sensortag_configs/$usb.ccxml\"}, ==> ocean.json"
-            echo "\"$stag_id\":{\"gateway\":\"$mac_mini\",\"port\":$port,\"serial_id\":\"$dev\",\"flash_file\":\"/home/username/sensortag_configs/$usb.ccxml\"}," >> ocean.json
+                    echo "\"$stag_id\":{\"gateway\":\"$mac_mini\",\"port\":$port,\"serial_id\":\"$dev\",\"flash_file\":\"/home/cirlab/sensortag_configs/$usb.ccxml\"}, ==> ocean.json"
+            echo "\"$stag_id\":{\"gateway\":\"$mac_mini\",\"port\":$port,\"serial_id\":\"$dev\",\"flash_file\":\"/home/cirlab/sensortag_configs/$usb.ccxml\"}," >> ocean.json
             echo "$port:raw:0:$dev:115200,8DATABITS,NONE,1STOPBIT ==> $conf_file_name"
             echo "$port:raw:0:$dev:115200,8DATABITS,NONE,1STOPBIT" >> $conf_file_name
             # echo "DELETE from motes WHERE virtual_id = $stag_id; ==> DB";
+            # echo "DELETE from motes WHERE virtual_id = $stag_id;" | mysql -uroot -pNUS_SOC_cirlab_indriya-2017 indriyaDB;
             # echo "INSERT INTO motes (virtual_id, moteTypes_moteTypeID, clusters_clusterID) values ($stag_id, 9, $mac_mini_db_id); ==> DB"
-            echo "INSERT INTO motes (virtual_id, moteTypes_moteTypeID, clusters_clusterID) values ($stag_id, 9, $mac_mini_db_id);" | mysql -uroot -pPASSWORD DATABASE_NAME;
+            echo "INSERT INTO motes (virtual_id, moteTypes_moteTypeID, clusters_clusterID) values ($stag_id, 9, $mac_mini_db_id);" | mysql -uroot -pNUS_SOC_cirlab_indriya-2017 indriyaDB;
             let "port++";
         done < $file_name
         echo ""
@@ -197,13 +201,19 @@ do
             echo ""
     done < mac-mini
 
+    cp ~/indriya_upgrade/nodes_virt_id_phy_id.json ~/indriya_upgrade/nodes_virt_id_phy_id.json.bak
+    echo "}" >> ocean.json
+    cp ocean.json ~/indriya_upgrade/nodes_virt_id_phy_id.json
+
     echo "run $x times"
     sleep 30
 done
 
-    #ssh username@mac-mini-com1-b-el '/home/username/indriya_upgrade/burn_telosb_test.py telosb #RECORD_FOR_SERIAL_DEVICE_FROM_FILE# /home/username/indriya_upgrade/telosb_bin/burn-nodeid.sky.#ID#' &
+    cd ~/indriya_upgrade & ./indriya.sh
+
+    #ssh cirlab@mac-mini-com1-b-el '/home/cirlab/indriya_upgrade/burn_telosb_test.py telosb #RECORD_FOR_SERIAL_DEVICE_FROM_FILE# /home/cirlab/indriya_upgrade/telosb_bin/burn-nodeid.sky.#ID#' &
     # example 
-    # ssh username@mac-mini-com1-b-el '/home/username/indriya_upgrade/burn_telosb_test.py telosb /dev/serial/by-id/usb-XBOW_Crossbow_Telos_Rev.B_XBR1PSTE-if00-port0 /home/username/indriya_upgrade/telosb_bin/burn-nodeid.sky.101' &
+    # ssh cirlab@mac-mini-com1-b-el '/home/cirlab/indriya_upgrade/burn_telosb_test.py telosb /dev/serial/by-id/usb-XBOW_Crossbow_Telos_Rev.B_XBR1PSTE-if00-port0 /home/cirlab/indriya_upgrade/telosb_bin/burn-nodeid.sky.101' &
     ######
 
     #################
@@ -211,21 +221,23 @@ done
     #################
     # INCREMENT #ID##, start from let's say 100 
     # NEXT RECORD FROM mac-mini-com1-b-mac.serial
-    #ssh username@mac-mini-com1-b-mac '/home/username/indriya_upgrade/burn_telosb_test.py telosb #RECORD_FOR_SERIAL_DEVICE_FROM_FILE# /home/username/indriya_upgrade/telosb_bin/burn-nodeid.sky.#ID#' &
+    #ssh cirlab@mac-mini-com1-b-mac '/home/cirlab/indriya_upgrade/burn_telosb_test.py telosb #RECORD_FOR_SERIAL_DEVICE_FROM_FILE# /home/cirlab/indriya_upgrade/telosb_bin/burn-nodeid.sky.#ID#' &
 
     #echo ""
     #echo "*** LOOP FOR NEXT FILE ***"
 
     #while read line_from_file;
-    #	do echo "Will execute: ssh username@mac-mini-com1-b-mac '/home/username/indriya_upgrade/burn_telosb_test.py telosb $line_from_file /home/username/indriya_upgrade/telosb_bin/burn-nodeid.sky.$ID' &";
-    #    ssh username@mac-mini-com1-b-mac "/home/username/indriya_upgrade/burn_telosb_test.py telosb $line_from_file /home/username/indriya_upgrade/telosb_bin/burn-nodeid.sky.$ID" &
+    #	do echo "Will execute: ssh cirlab@mac-mini-com1-b-mac '/home/cirlab/indriya_upgrade/burn_telosb_test.py telosb $line_from_file /home/cirlab/indriya_upgrade/telosb_bin/burn-nodeid.sky.$ID' &";
+    #    ssh cirlab@mac-mini-com1-b-mac "/home/cirlab/indriya_upgrade/burn_telosb_test.py telosb $line_from_file /home/cirlab/indriya_upgrade/telosb_bin/burn-nodeid.sky.$ID" &
     #    echo "\"$ID\":{\"gateway\":\"mac-mini-com1-b-mac\",\"port\":$port,\"serial_id\":\"$line_from_file\",\"flash_file\":\"\"},"  >> ocean.json
     #	echo "$port:raw:0:$line_from_file:115200,8DATABITS,NONE,1STOPBIT" >> ser2net_mac-mini-com1-b-mac.conf
+    #	echo "DELETE from motes WHERE virtual_id = $ID;" | mysql -uroot -pNUS_SOC_cirlab_indriya-2017 indriyaDB;
+    #	echo "INSERT INTO motes (virtual_id, moteTypes_moteTypeID, clusters_clusterID) values ($ID, 8, 7);" | mysql -uroot -pNUS_SOC_cirlab_indriya-2017 indriyaDB;
     #	let "ID++";
     #	let "port++";
     #done < mac-mini-com1-b-mac.serial
-    #echo "Will execute: scp ser2net_mac-mini-com1-b-mac.conf username@mac-mini-com1-b-mac:/home/username/indriya_upgrade/"
-    #scp ser2net_mac-mini-com1-b-mac.conf username@mac-mini-com1-b-mac:/home/username/indriya_upgrade/
+    #echo "Will execute: scp ser2net_mac-mini-com1-b-mac.conf cirlab@mac-mini-com1-b-mac:/home/cirlab/indriya_upgrade/"
+    #scp ser2net_mac-mini-com1-b-mac.conf cirlab@mac-mini-com1-b-mac:/home/cirlab/indriya_upgrade/
 
 
     # 4030:raw:0:/dev/serial/by-id/usb-Texas_Instruments_XDS110__02.02.05.01__with_CMSIS-DAP_L1016-if00:115200,8DATABITS,NONE,1STOPBIT
