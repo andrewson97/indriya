1268a009dbcacb81ce1c14b7a36bcb434317ab57
Andrew <58026793+andrewson97@users.noreply.github.com>
2022-06-13 03:18:51 +0530

    fix on database connection

--- scripts/server.py
+++ scripts/server.py
@@ -4,12 +4,12 @@ import os
 
 
 def install_essentials():
-    cmd = ["sudo apt update", "sudo apt install apache2",
+    cmd = ["sudo apt-get update -y", "sudo apt install apache2",
     "sudo ufw enable", "sudo ufw allow 'Apache'",
-    "sudo apt-get install php7.2", "sudo apt install mysql-server",
+    "sudo apt-get install php7.4", "sudo apt install mysql-server",
     "sudo systemctl start mysql.service","sudo apt install git",
-    "sudo apt install python3-pip",
-    "pip3 install GitPython",]
+    "sudo apt install python3-pip","sudo apt-get install -y php-mysqli"
+    "pip3 install GitPython", "pip install mysql-connector-python"]
     for cmds in cmd :
         args = shlex.split(cmds)
         subprocess.Popen(args).communicate("password")
@@ -33,11 +33,13 @@ def download_essentials():
 
 def apache_setup1():
     path_a = get_path()
-    cmd =["chmod +x " + path_a + "/virtualhost/virtualhost.sh",
+    cmd =["sudo ln -s /var/www " + path_a + "/www",
+    "sudo chmod +x " + path_a + "/virtualhost/virtualhost.sh",
     "sudo cp " + path_a + "/virtualhost/virtualhost.sh /usr/local/bin/virtualhost"]
     for cmds in cmd :
         args = shlex.split(cmds)
         subprocess.Popen(args).communicate("password")
+    
 
 def apache_setup2():
     action = input("create or delete :")
@@ -46,29 +48,27 @@ def apache_setup2():
     #arg = shlex.split("sudo virtualhost " + action + " " + domain + " " + host_dir)
     subprocess.run(["sudo", "virtualhost" , action , domain , host_dir])
     if action == "create":
-        print ("\nvirtual host sucsusfully setup\n coppying needed files to the directory")
         place_files(host_dir)
-        cmd1 = "sudo chown -R $USER:$USER /var/www"
-        cmd2 = "sudo chmod -R 755 /var/www"
-        cmd3 = "sudo chmod 777 /var/www/" + host_dir + "/conf.d/indriya_db_pass"
-        os.system(cmd1)
-        os.system(cmd2)
-        os.system(cmd3)
     return str(host_dir)
 
 def place_files(host_dir):
     path_a = get_path()
     cmd = "sudo cp -RT " + path_a + "/indriya-master/server/frontend/website " + path_a + "/www/" + host_dir
-    arg = shlex.split(cmd)
-    subprocess.run(arg)
-    """cmd1 = "sudo cp -RT" + path_a + "/indriya-master/server/frontend/website " + path_a + "/www/" + host_dir
-    x = os.system(cmd1)
-    if x == 0 : print ("files coppied sucsussfully")"""
+    cmd1= "sudo cp -R " + path_a + "/indriya-master/server/frontend/website/conf.d " + path_a + "/www/conf.d"
+    os.system(cmd)
+    os.system(cmd1)
 
 
 def sql_setup():
     cmds = ["sudo systemctl start mysql.service",
     "sudo mysql_secure_installation",]
+
+    # if you get this error:
+    # "Failed! Error: SET PASSWORD has no significance for user ‘root’@’localhost’ as 
+    # the authentication method used doesn’t store authentication data in the MySQL server" 
+    # go to terminal and go to mysql using 'sudo mysql' command and enter this command in mysql 
+    # 'ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password by 'admin''
+
     for cmd in cmds:
         arg = shlex.split(cmd)
         subprocess.run(arg)
@@ -79,17 +79,23 @@ def sql_setup():
 def write_essentials(sqlpwd,host_dir):
     path_a = get_path()
     path = path_a + "/www/" + host_dir + "/conf.d/indriya_db_pass"
+    path1 = path_a + "/www/conf.d/indriya_db_pass"
     #d_path = path_a + "/www/conf.d/indriya_db_pass"
+    os.system("sudo chown -R $USER:$USER /var/www")
+    os.system("sudo chmod -R +rwx /var/www")
     f = open(path, "w")
     f.write(sqlpwd)
     f.close()
+    ff = open(path1, "w")
+    ff.write(sqlpwd)
+    ff.close()
     #fd = open(d_path)
     #fd.write(sqlpwd)
     #fd.close()
 
 def create_db():
     path_a = get_path()
-    cmd = "sudo mysql -u root -p < " + path_a + "/indriya-master/server/frontend/database/indriyaDB.sql"
+    cmd = "mysql -u root -p < " + path_a + "/indriya-master/server/frontend/database/indriyaDB.sql"
     #print(cmd)
     #arg = shlex.split(cmd)
     #print(arg)
@@ -98,12 +104,14 @@ def create_db():
     if x == 0 : print ("DB created Sucusfully")
 
 
-install_essentials()
-download_essentials()
-apache_setup1()
-host_dir = apache_setup2()
-#sqlpwd = sql_setup()
-#host_dir = "indriya"
-sqlpwd = "admin"
-write_essentials(sqlpwd,host_dir)
-create_db()
+# install_essentials()
+# download_essentials()
+# apache_setup1()
+# host_dir = apache_setup2()
+# sqlpwd = sql_setup()
+# host_dir = "test"
+# sqlpwd = "admin"
+# write_essentials(sqlpwd,host_dir)
+# create_db()
+
+
